0433abfbfa0caae6884b7dd7233c4fe3687e3476
Ttkode <thameemadz@gmail.com>
2022-12-06 02:36:19 +0530

    added triangleArea.js

---
+++ triangleArea.js
@@ -0,0 +1,15 @@
+var inputs = document.querySelectorAll(".input-length");
+var calculateAreaBtn = document.querySelector("#calculate-area-btn");
+var outputDiv = document.querySelector("#output-div");
+
+function calculateArea(base, height){
+    var area = 0.5 * base * height;
+    return area;
+}
+
+function displayArea(){
+    var area = calculateArea(Number(inputs[0].value), Number(inputs[1].value));
+    outputDiv.innerText = "The area of triangle is " + area + "cm²";
+}
+
+calculateAreaBtn.addEventListener("click", displayArea);
